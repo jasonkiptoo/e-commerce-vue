d00f34cc0fd256bb1c32be606b5f1c637f954aae
jasonkiptoo <kiptoojason@gmail.com>
2024-03-20 21:24:53 +0300

    feature uppdate

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,12 @@
     <div>
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4 flex justify-between">
-                <NuxtLink to="/" class="font-bold">Home Bee</NuxtLink>
+                <NuxtLink to="/" class="font-bold">Home Bee 🐝</NuxtLink>
                 <ul class="flex gap-4">
-                 <li><NuxtLink to="/">Home</NuxtLink></li>
+                 <li><NuxtLink to="/" class="btn">Home</NuxtLink></li>
                  <li><NuxtLink to="/about">About</NuxtLink></li>
                  <li><NuxtLink to="/products">Products</NuxtLink></li>
-                 <li><NuxtLink to="/contact">Contact Us</NuxtLink></li>
+                 <li><NuxtLink to="/contact" class="btn">Contact Us</NuxtLink></li>
                 </ul>
             </nav>
         </header>
