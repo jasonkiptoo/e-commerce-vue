395957adffa58ce8ed2916f1fa2c1b94d7e84d39
jasonkiptoo <kiptoojason@gmail.com>
2024-03-20 22:00:02 +0300

    changes in  css

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,10 @@
             <nav class="container mx-auto p-4 flex justify-between">
                 <NuxtLink to="/" class="font-bold">Home Bee 🐝</NuxtLink>
                 <ul class="flex gap-4">
-                 <li><NuxtLink to="/" class="btn">Home</NuxtLink></li>
+                 <li><NuxtLink to="/" class="">Home</NuxtLink></li>
                  <li><NuxtLink to="/about">About</NuxtLink></li>
                  <li><NuxtLink to="/products">Products</NuxtLink></li>
-                 <li><NuxtLink to="/contact" class="btn">Contact Us</NuxtLink></li>
+                 <li><NuxtLink to="/contact" class="btn">Sign In</NuxtLink></li>
                 </ul>
             </nav>
         </header>
