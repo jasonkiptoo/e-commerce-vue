24fe3f74871320cd431e655d8371638221eb5664
jasonkiptoo <kiptoojason@gmail.com>
2024-03-20 19:27:59 +0300

    D

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,17 @@
-/about
+<template>
+    <div>
+        <h2>About us</h2>
+        <p>About us</p>
+
+
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,17 @@
-Home
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Welcome Home</p>
+
+
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
