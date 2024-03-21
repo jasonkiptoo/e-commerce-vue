a42b4ef110fa7b3e3b3ffacee2125c522d751128
jasonkiptoo <kiptoojason@gmail.com>
2024-03-21 20:48:19 +0300

    updatye on product details

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,27 +1,27 @@
 <template>
-    <div>
+   <div class="card">
+    <div class="grid grid-cols-2 gap-10">
+        <div class="p-7">
+            <img :src="product.image"  alt="product img" class="mx-auto my-7">
+        </div>
 
-        <div class="container mx-auto flex pl-">
- 
- <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-<img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" :src="product.image" alt="">
-<div class="flex flex-col justify-between p-4 leading-normal">
- <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{product.title}}</h5>
- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ product.description }}</p>
-</div>
-</a>
-
-</div>
-
-
+        <div class="py-7">
+            <h2 class="text-ful-xl my-7">{{ product.title }}</h2>
+            <p class="text-xl my-7">Price -${{ product.price }}</p>
+            <h3 class="font-bold border-b-2 mb-4 pb-2">Product Description</h3>
+            <p class="text-xm">{{ product.description }}</p>
+        </div>
     </div>
- 
+   </div>
 </template>
 
 <script setup>
 const {product}= defineProps(['product'])
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+img{
+    max-width: 400px;
+}
 
 </style>
